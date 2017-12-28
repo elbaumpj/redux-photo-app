2f3154eff6f2e3a95c41b5046c65258fa421f7d9
elbaumpj <elbaumpj@gmail.com>
2017-12-28 17:38:47 -0500

    comments display working

--- client/components/Single.js
+++ client/components/Single.js
@@ -4,14 +4,18 @@ import Comments from './Comments';
 
 class Single extends React.Component {
     render() {
+        //post ID
+        const { postId } = this.props.params; 
         //index of post
-        const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId); 
+        const i = this.props.posts.findIndex((post) => post.code === postId); 
         // post itself
         const post = this.props.posts[i];  
+        //comments 
+        const postComments = this.props.comments[postId] || []; 
         return (
             <div className="single-photo">
                 <Photo i={i} post={post} {...this.props}    />
-                <Comments /> 
+                <Comments postComments={postComments} /> 
             </div>
         );
     }
